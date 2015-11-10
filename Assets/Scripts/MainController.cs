d2f2ae977365da29c9a1332e1c23cdf377ec996f
Tentaculat <vladimir.anishchik@gmail.com>
2015-11-10 19:42:21 +0600

    First screen with "Enter" and "Exit" buttons was implemented

---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainController : MonoBehaviour {
+    public string gameSceneName = "game";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnEnterButton() {
+        Application.LoadLevel(gameSceneName);
+    }
+
+    public void onExitButton() {
+        Application.Quit();
+    }
+}

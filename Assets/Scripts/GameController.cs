a70fe76f40b0de9c76c06af1cc311ae041ee17cc
Tentaculat <vladimir.anishchik@gmail.com>
2015-11-11 19:22:47 +0600

    Ball emitter, ball and paddle added

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+    public Transform paddle;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            paddle.Translate(-100 * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            paddle.Translate(100 * Time.deltaTime, 0, 0);
+        }
+
+    }
+}
